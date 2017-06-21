99286a7d002ebe863f407f58ae3a91a032cd4198
Rui Ueyama <ruiu@google.com>
2017-06-21 15:08:47 -0700

    First commit.

---
+++ Makefile
@@ -0,0 +1,2 @@
+undname: MicrosoftDemangle.o
+	$(CXX) -o $@ $?
---
+++ MicrosoftDemangle.cpp
@@ -0,0 +1,20 @@
+//===- ItaniumDemangle.cpp ------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <numeric>
+#include <string>
+#include <vector>
+
+int main(int argc, char **argv) {
+  return 0;
+}
